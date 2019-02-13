19105903e572fbe6286fb67a8562bbc1883ae18c
cb4ds <connie@aggregate-genius.com>
2019-02-13 17:08:04 +0000

    kaplan-meijer issues

--- tests/testthat/test-ui-kaplanmeier.R
+++ tests/testthat/test-ui-kaplanmeier.R
@@ -4,19 +4,19 @@ context("canvasXpress Web Charts - Kaplanmeier")
 test_that("cXkaplanmeier1", {
     check_ui_test(cXkaplanmeier1())
 
-    warning("Plot looks very different compared to web example (afterRender seems not to work)")
+    fail("Plot looks very different compared to web example (afterRender seems not to work)")
 })
 
 test_that("cXkaplanmeier2", {
     check_ui_test(cXkaplanmeier2())
 
-    warning("Plot looks very different compared to web example (afterRender seems not to work)")
+    fail("Plot looks very different compared to web example (afterRender seems not to work)")
 })
 
 test_that("cXkaplanmeier3", {
     check_ui_test(cXkaplanmeier3())
 
-    warning("The lines look different compared to web example (e.g. the 'Drug1' line")
+    fail("The lines look different compared to web example (e.g. the 'Drug1' line")
 })
 
 test_that("cXkaplanmeier4", {
