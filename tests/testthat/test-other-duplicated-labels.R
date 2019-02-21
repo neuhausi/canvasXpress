0867fef36a64b3f5b49b8b842dd8e586d2a2528f
cb4ds <connie@aggregate-genius.com>
2019-02-21 16:54:10 +0000

    added boxplot test

--- tests/testthat/test-other-duplicated-labels.R
+++ tests/testthat/test-other-duplicated-labels.R
@@ -24,16 +24,24 @@ test_that("barplot duplicated labels", {
 
 test_that("boxplot duplicated labels", {
 
-    colnames(y) <- c("S1", "S1", "S1", "S1", "S0", "S1", "S4", "S4")
-    rownames(x) <- colnames(y)
+    precalc.data <- data.frame(iqr1   = c(45,   7.3, 8, NA),     qtl1     = c(109, 14.9, 4, NA),
+                               median = c(159, 20.1, 7.25, NA),  qtl3     = c(249, 26.2, 11.3, NA),
+                               iqr3   = c(337, 40.6, 17.4, NA),  outliers = c("", "44.5, 46", "", ""),
+                               type   = c("A", "B", "C", "D"),
+                               stringsAsFactors = F)
 
-    result <- canvasXpress(
-        data                      = y,
-        smpAnnot                  = x,
-        graphOrientation          = "vertical",
-        graphType                 = "Boxplot",
-        title                     = "Duplicated labels should appear correct",
-        xAxis2Show                = FALSE
-    )
+    precalc.data <- as.data.frame(t(precalc.data))
+    colnames(precalc.data) <- c("Assault", "Rape", "Assault", "Missing")
+
+    result <- canvasXpress(data                  = precalc.data,
+                           smpAnnot              = precalc.data["type", ],
+                           graphType             = "Boxplot",
+                           graphOrientation      = "vertical",
+                           smpLabelFontStyle     = "italic",
+                           smpLabelRotate        = 90,
+                           showLegend            = FALSE,
+                           title                 = "Precalculated boxplot - data with smpAnnot",
+                           titleScaleFontFactor  = 0.5)
+
     check_ui_test(result)
 })
