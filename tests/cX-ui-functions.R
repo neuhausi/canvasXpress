3998289614dae2649acd480d0739bc1ff2e69af0
cb4ds <connie@aggregate-genius.com>
2019-05-22 17:38:22 +0000

    ready for testing 24.0 CX

--- tests/cX-ui-functions.R
+++ tests/cX-ui-functions.R
@@ -4423,7 +4423,7 @@ cXtreemap3 <- function() {
 cXvenn1 <- function() {
   library(canvasXpress)
   canvasXpress(
-    vennData=data.frame(ABC=552, B=562, ABD=578, BD=354, ABCD=148, AD=257, ACD=298, AC=456, BC=915, AB=639, C=620, CD=143, BCD=613, A=340, D=592),
+    vennData=data.frame(AC=456, ABCD=148, BC=915, ABC=552, CD=143, ABD=578, AD=257, BCD=613, BD=354, ACD=298, C=620, AB=639, B=562, A=340, D=592),
     vennLegend=list(A="List 1", B="List 2", C="List 3", D="List 4"),
     graphType="Venn",
     showTransition=TRUE,
@@ -4434,7 +4434,7 @@ cXvenn1 <- function() {
 cXvenn2 <- function() {
   library(canvasXpress)
   canvasXpress(
-    vennData=data.frame(AC=456, ACD=298, AD=257, ABCD=148, BD=354, ABD=578, B=562, ABC=552, D=592, BCD=613, A=340, CD=143, C=620, BC=915, AB=639),
+    vennData=data.frame(D=592, A=340, B=562, C=620, AB=639, AD=257, ABD=578, BCD=613, BD=354, ACD=298, BC=915, ABC=552, CD=143, ABCD=148, AC=456),
     vennLegend=list(A="List 1", B="List 2", C="List 3", D="List 4"),
     graphType="Venn",
     showTransition=TRUE,
@@ -4445,7 +4445,7 @@ cXvenn2 <- function() {
 cXvenn3 <- function() {
   library(canvasXpress)
   canvasXpress(
-    vennData=data.frame(CD=143, D=592, A=340, BCD=613, C=620, AB=639, BC=915, ACD=298, AC=456, ABC=552, ABCD=148, AD=257, ABD=578, BD=354, B=562),
+    vennData=data.frame(AB=639, C=620, ACD=298, BCD=613, BD=354, ABD=578, AD=257, ABC=552, CD=143, BC=915, AC=456, ABCD=148, D=592, A=340, B=562),
     vennLegend=list(A="List 1", B="List 2", C="List 3", D="List 4"),
     graphType="Venn",
     vennGroups=2
