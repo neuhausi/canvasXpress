6244021aa7578bb9ac21889a91d69ce7de21c0fc
angeline-pro <48724863+angeline-pro@users.noreply.github.com>
2019-09-23 10:20:59 -0400

    testing 25.3 (#101)
    * testing 25.3, stopped after gantt
    
    * removed table color warnings

--- tests/testthat/test-ui-chord.R
+++ tests/testthat/test-ui-chord.R
@@ -11,4 +11,5 @@ test_that("cXchord2", {
 
 test_that("cXchord3", {
     check_ui_test(cXchord3())
+    warning("when hovering over graph, text is displayed in the plot centre (if plot is not resized)")
 })
