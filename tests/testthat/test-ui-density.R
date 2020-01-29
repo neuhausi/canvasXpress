b394e0c3fa59116481264b4dcfe2572be42ebf87
ginberg <ginberg@gmail.com>
2020-01-29 23:20:09 +0100

    test updates (#112)
    * 26.8 test results
    
    * test updates

--- tests/testthat/test-ui-density.R
+++ tests/testthat/test-ui-density.R
@@ -3,30 +3,44 @@ context("canvasXpress Web Charts - Density")
 
 test_that("cXdensity1", {
     check_ui_test(cXdensity1())
+
+    warning("Clicking on legend doesn't fade clicked item")
 })
 
 test_that("cXdensity2", {
     check_ui_test(cXdensity2())
+
+    warning("Clicking on legend doesn't fade clicked item")
 })
 
 test_that("cXdensity3", {
     check_ui_test(cXdensity3())
+
+    warning("Clicking on legend doesn't fade clicked item")
 })
 
 test_that("cXdensity4", {
     check_ui_test(cXdensity4())
+
+    warning("Clicking on legend doesn't fade clicked item")
 })
 
 test_that("cXdensity5", {
     check_ui_test(cXdensity5())
+
+    warning("Clicking on legend doesn't fade clicked item")
 })
 
 test_that("cXdensity6", {
     check_ui_test(cXdensity6())
+
+    warning("Clicking on legend doesn't fade clicked item")
 })
 
 test_that("cXdensity7", {
     check_ui_test(cXdensity7())
+
+    warning("Clicking on legend doesn't fade clicked item")
 })
 
 test_that("cXdensity8", {
--- tests/testthat/test-ui-pie.R
+++ tests/testthat/test-ui-pie.R
@@ -3,13 +3,9 @@ context("canvasXpress Web Charts - Pie")
 
 test_that("cXpie1", {
     check_ui_test(cXpie1())
-
-    warning ("Hover results in the color of the pie chart changing until the plot is resized")
 })
 
 test_that("cXpie2", {
     check_ui_test(cXpie2())
-
-    warning ("Hover results in the color of the pie chart changing until the plot is resized")
 })
 
