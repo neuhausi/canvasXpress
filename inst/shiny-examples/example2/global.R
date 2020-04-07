e2d8a4c07c7e6f992f82aeaa0ace0df7bea17f4f
saranya-ag <51917619+saranya-ag@users.noreply.github.com>
2020-04-08 00:39:59 +0530

    Cran (#125)

--- inst/shiny-examples/example2/global.R
+++ inst/shiny-examples/example2/global.R
@@ -15,8 +15,6 @@ g_GSE9750$x[is.na(g_GSE9750$x)] <- "<NA>"
 
 # custom sorting for gene drop-down
 g_geneChoices      <- setNames(rownames(g_GSE9750$y), g_GSE9750$z[rownames(g_GSE9750$y), 'Symbol'])
-g_geneChoices      <- g_geneChoices[c(which(grepl('\\(', names(g_geneChoices)))[1]:length(g_geneChoices),
-                                      1:which(grepl('\\C', names(g_geneChoices)))[1]-1)]
 g_geneChoices.sort <- g_geneChoices[sort(names(g_geneChoices))]
 g_geneChoices.splt <- grep('^A', names(g_geneChoices.sort))[1]
 g_geneChoices      <- g_geneChoices.sort[c(g_geneChoices.splt:length(g_geneChoices), 1:(g_geneChoices.splt - 1))]
--- tests/testthat/test-ui-oncoprint.R
+++ tests/testthat/test-ui-oncoprint.R
@@ -15,4 +15,7 @@ test_that("cXoncoprint3", {
 
 test_that("cXoncoprint4", {
     check_ui_test(cXoncoprint4())
+
+    warning("The values on Tooltips are different from web. In the R version, there are NaN values while the web has zeroes. ",
+             "This is due to the differences in the underlying data")
 })
