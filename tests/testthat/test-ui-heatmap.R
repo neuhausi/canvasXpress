8d8b43d631cfd2de3daa46b8b57101b4677e034d
kartikeya kirar <49568503+kar-agg-gen@users.noreply.github.com>
2020-05-05 20:13:49 +0530

    Cran (#132)

--- tests/testthat/test-ui-heatmap.R
+++ tests/testthat/test-ui-heatmap.R
@@ -42,6 +42,8 @@ test_that("cXheatmap9", {
 
 test_that("cXheatmap10", {
     check_ui_test(cXheatmap10())
+
+    message('Plot points all fade on selecting shapeBy and sizeBy legend items - working as designed.')
 })
 
 test_that("cXheatmap11", {
