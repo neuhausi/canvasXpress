b94502cce3da3efdc6db15d973dc4b43732ac5b6
dheerajagr7 <63011169+dheerajagr7@users.noreply.github.com>
2020-07-03 12:04:00 +0530

    Changes in Contour function names (#140)
    * corrected function names

--- tests/testthat/test-ui-contour.R
+++ tests/testthat/test-ui-contour.R
@@ -6,17 +6,17 @@ test_that("cXcontour1", {
 })
 
 test_that("cXcontour2", {
-    check_ui_test(cXcontour1())
+    check_ui_test(cXcontour2())
 })
 
 test_that("cXcontour3", {
-    check_ui_test(cXcontour1())
+    check_ui_test(cXcontour3())
 })
 
 test_that("cXcontour4", {
-    check_ui_test(cXcontour1())
+    check_ui_test(cXcontour4())
 })
 
 test_that("cXcontour5", {
-    check_ui_test(cXcontour1())
+    check_ui_test(cXcontour5())
 })
