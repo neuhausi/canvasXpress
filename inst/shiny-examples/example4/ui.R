e3bb9650a4bfa7bbd389a09e7dd1730a3bc45fe7
Jennifer Walker <90073076+agenius-jennifer@users.noreply.github.com>
2023-06-09 08:38:24 -0700

    shiny example 4 minor code style updates (#188)

--- inst/shiny-examples/example4/ui.R
+++ inst/shiny-examples/example4/ui.R
@@ -4,13 +4,13 @@ library(DT)
 
 
 shinyUI(
-  fluidPage(
-    h1("CX events heatmap example"),
-    h3("Double click heatmap cell to highlight table cell"),
-    canvasXpress::canvasXpressOutput("genelist_heatmap",
-      width = "100%",
-      height = "250px"
-    ),
-    DTOutput("y")
-  )
+    fluidPage(
+        h1("CX events heatmap example"),
+        h3("Double click heatmap cell to highlight table cell"),
+        canvasXpress::canvasXpressOutput("genelist_heatmap",
+                                         width  = "100%",
+                                         height = "250px"
+        ),
+        DTOutput("y")
+    )
 )
