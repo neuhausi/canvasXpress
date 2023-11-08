c68ab124e149dd767887d88ca4fedab0715bece1
cb4ds <connie@aggregate-genius.com>
2023-11-08 09:15:32 -0800

    bugfix by Carson for HTMLwidgets 1.6.3

--- tests/testthat/test-other--BASE.R
+++ tests/testthat/test-other--BASE.R
@@ -89,9 +89,9 @@ test_that("Shiny Examples", {
 
 
 test_that("Shiny Input", {
-    skip_if_not_installed("htmlwidgets", "1.6")
+    skip_if_not_installed("htmlwidgets", "1.6.3")
+    skip_on_cran()
 
     expect_equal(as.character(canvasXpressOutput("test_id")),
-                 '<div class="canvasXpress html-widget html-widget-output shiny-report-size html-fill-item-overflow-hidden html-fill-item" id="test_id" style="width:100%;height:400px;"></div>')
+                 '<div class="canvasXpress html-widget html-widget-output shiny-report-size html-fill-item" id="test_id" style="width:100%;height:400px;"></div>')
 })
-
