538e053636b610c44c1c5ab7a53f7d73511ee007
cb4ds <connie@aggregate-genius.com>
2024-06-07 11:26:31 -0700

    50.2 for testing

--- revdep/run_job.R
+++ revdep/run_job.R
@@ -1,4 +1,7 @@
 # Script to Run In A Separate Job
 
+# uncomment to reset
+# revdepcheck::revdep_reset()
+
 library(revdepcheck)
 revdep_check(quiet = FALSE, num_workers = 2, pkg = ".", timeout = as.difftime(120, units = "mins"))
