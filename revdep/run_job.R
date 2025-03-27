f1a45e3fbee4352e22d1ad72541660aecb9ff960
cb4ds <connie@aggregate-genius.com>
2025-03-27 09:35:38 -0700

    ready for CRAN release

--- revdep/run_job.R
+++ revdep/run_job.R
@@ -1,7 +1,8 @@
 # Script to Run In A Separate Job
 
+library(revdepcheck)
+
 # uncomment to reset
 # revdepcheck::revdep_reset()
 
-library(revdepcheck)
 revdep_check(quiet = FALSE, num_workers = 2, pkg = ".", timeout = as.difftime(120, units = "mins"))
